6e90bdccf39e1d9fb5204b4d3251e76fcb09fcae
entwan <entwan@gmail.com>
2022-06-16 17:08:10 +0200

    Ap implementation AdapterFilms HomeActivity => ajoute bien dans le Firestore Database les films => ajoute bien dans le Storage les affiches => charge bien la liste des films dans le recycler View => avec toolbar

--- app/src/main/java/com/dam/gmdb/HomeActivity.java
+++ app/src/main/java/com/dam/gmdb/HomeActivity.java
@@ -1,6 +1,8 @@
 package com.dam.gmdb;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -9,6 +11,9 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.AttributeSet;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 import com.google.firebase.auth.FirebaseAuth;
@@ -25,7 +30,7 @@ public class HomeActivity extends AppCompatActivity {
     private RecyclerView rvFilms;
     private AdapterFilms adapterFilms;
     private FirebaseFirestore db;
-
+    private Toolbar toolbar;
 
     //methode d'initialisation
     private void init() {
@@ -38,11 +43,71 @@ public class HomeActivity extends AppCompatActivity {
         //reserve taille
         rvFilms.setHasFixedSize(true);
 
-        rvFilms.setLayoutManager(new LinearLayoutManager(context,
+        rvFilms.setLayoutManager(new LinearLayoutManagerWrapper(context,
                 LinearLayoutManager.VERTICAL, false));
-        //rvFilms.setItemAnimator(new DefaultItemAnimator());
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         db = FirebaseFirestore.getInstance();
+
+    }
+
+    public class LinearLayoutManagerWrapper extends LinearLayoutManager {
+
+        public LinearLayoutManagerWrapper(Context context) {
+            super(context);
+        }
+
+        public LinearLayoutManagerWrapper(Context context, int orientation, boolean reverseLayout) {
+            super(context, orientation, reverseLayout);
+        }
+    }
+
+    /* La gestion du menu */
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // La liaison avec le layout du menu
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+
+        // Liaison avec le widget de recherche
+        final MenuItem searchItem = menu.findItem(R.id.action_search);
+        final SearchView searchView = (SearchView) searchItem.getActionView();
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                searchFilm(newText);
+                return false;
+            }
+        });
+
+
+        return true;
+    }
+
+    private void searchFilm(String s) {
+        Query query = db.collection(TABLE_FILM);
+        if (!String.valueOf(s).equals("")) {
+            query = query
+                    .orderBy(KEY_TITRE_MINUSCULE)
+                    .startAt(s)
+                    .endAt(s + "\uf8ff");
+        }
+        FirestoreRecyclerOptions<ModelFilms> searchFilm =
+                new FirestoreRecyclerOptions.Builder<ModelFilms>()
+                        .setQuery(query, ModelFilms.class)
+                        .build();
+
+        adapterFilms = new AdapterFilms(searchFilm);
+        rvFilms.setAdapter(adapterFilms);
+        adapterFilms.startListening();
 
     }
 
@@ -63,8 +128,8 @@ public class HomeActivity extends AppCompatActivity {
     /* Verifier si les datas sont presentes dans la db */
     private void addSampleData() {
         SharedPreferences sharedPreferences = getSharedPreferences(R.class.getPackage().getName()
-        + ".prefs", Context.MODE_PRIVATE);
-        if(!sharedPreferences.getBoolean(UPLOAD_PREFS, false)) {
+                + ".prefs", Context.MODE_PRIVATE);
+        if (!sharedPreferences.getBoolean(UPLOAD_PREFS, false)) {
             AddSampleDatasToFirebase.addDatasToFireBase(getApplicationContext());
         }
     }
@@ -85,7 +150,7 @@ public class HomeActivity extends AppCompatActivity {
         super.onStart();
 
         FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
-        if(currentUser==null) {
+        if (currentUser == null) {
             startActivity(new Intent(HomeActivity.this, SignInActivity.class));
         } else {
             adapterFilms.startListening();
